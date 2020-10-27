45dc970d646cfc72cf1fbb7bf24c95edf1b4fbaa
Jung Lee <lee.jung.s95@gmail.com>
2020-10-27 04:17:56 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,5 +15,8 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     binding.pry
+    self.artists.all.map do |artist|
+        artist.name
+    end
   end
 end
