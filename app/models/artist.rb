1d86ef569e0f0b73c4bab3bacb4ce2a49d196a15
Jung Lee <lee.jung.s95@gmail.com>
2020-10-27 04:12:46 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,6 +9,7 @@ class Artist < ActiveRecord::Base
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
     binding.pry
+    get_first_song.genres
   end
 
   def song_count
